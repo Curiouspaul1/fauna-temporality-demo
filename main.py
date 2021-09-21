5206e0248404620c6f7e0807096d78b089af1831
Curiouspaul1 <170408025@live.unilag.edu.ng>
2021-09-21 18:08:00 +0100

    completed demo

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 from faunadb.client import FaunaClient
+from faunadb.errors import FaunaError, HttpError
 from faunadb import query as q
 from dotenv import load_dotenv
+from typing import Optional
 import os
 
 load_dotenv()
 
 client = FaunaClient(secret=os.getenv('Fauna-secret'))
 
-# # reviewing customer data
-# customers = client.query(
-#     q.map_(
-#         lambda x: q.get(x),
-#         q.paginate(
-#             q.documents(q.collection('customers'))
-#         )
-#     )
-# # )
 
-# print(customers)
-
-data = client.query(
-    q.at(
-        q.time("1628815097906000"),
+def introspect(ref_id: str):
+    events_ = client.query(
         q.paginate(
-            q.ref("customers")
+            q.events(
+                q.ref(
+                    q.collection('customers'), ref_id
+                )
+            )
         )
     )
-)
+    return events_
 
-print(data)
+
+def downgrade(ref_id: str, steps: Optional[int] = -2):
+    events_ = introspect(ref_id)
+    # fetch previous state and update document with it instead
+    try:
+        client.query(
+            q.update(
+                q.ref(
+                    q.collection("customers"), ref_id
+                ),
+                {
+                    'data': events_['data'][steps]['data']
+                }
+            )
+        )
+    except FaunaError as e:
+        return "An error occurred while trying to update object, try again."
+    return "downgraded object successfully"
+
+print(introspect("102")) # inspect the history of the document before running the downgrade
+print(downgrade("102")) 
